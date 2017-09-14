4609367f31be579b09214a4afcb862761cd97032
Chet <chethana96@gmail.com>
2017-09-15 08:46:26 +1200

    added rellax js for paralax scrolling, still needs to be properly implimented

--- src/app/samples.component/posters.component/posters.component.ts
+++ src/app/samples.component/posters.component/posters.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 export class PostersComponent {
   title = 'Posters';  
 
+  private images = [
+    {
+      title: "80th Birthday",
+      path: "../../assets/img/samples/posters/80th-Birthday.jpg",
+      desc: "A sepia tone Welcome sign for an 80th birthday event",
+      pos: "left",
+      textPos: "right",
+      
+    }
+  ]
 }
